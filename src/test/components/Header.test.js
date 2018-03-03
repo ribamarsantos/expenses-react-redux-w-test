32722611003c0331d0ad6a9512dd97065096f252
Ribamar Santos <ribamaar@gmail.com>
2018-03-03 07:34:05 -0300

    updating to react v 16 and setting up enzyme

--- src/test/components/Header.test.js
+++ src/test/components/Header.test.js
@@ -1,11 +1,13 @@
 // Shallow render and FUll dom rendering tests for components
 import React from 'react'
-import ReactShallowRenderer from 'react-test-renderer/shallow'
+import { shallow } from 'enzyme'
 import Header from '../../components/Header'
 
 test('Should render Header correctly', () => {
-    const renderer = new ReactShallowRenderer();
-    renderer.render(<Header />);
-    expect(renderer.getRenderOutput()).toMatchSnapshot(); // compare with the first snapshot
+    const wrapper = shallow(<Header />)
+    expect(wrapper.find('h1').text()).toBe('Expensify ');
+    // const renderer = new ReactShallowRenderer();
+    // renderer.render(<Header />);
+    // expect(renderer.getRenderOutput()).toMatchSnapshot(); // compare with the first snapshot
 })
 
