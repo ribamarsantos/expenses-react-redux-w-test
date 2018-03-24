3ccc466b96065caae562fe638a7f997ffac9f616
Ribamar Santos <ribamaar@gmail.com>
2018-03-24 16:27:32 -0300

    refator and avoid DRY

--- src/test/components/AddExpensePage.test.js
+++ src/test/components/AddExpensePage.test.js
@@ -3,17 +3,21 @@ import { shallow } from 'enzyme'
 import { AddExpensePage } from '../../components/AddExpensePage';
 import expenses from '../fixtures/expenses';
 
+let onSubmit, history, wrapper;
+
+beforeEach( () => {
+     onSubmit = jest.fn();
+     history = { push: jest.fn()}
+     wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />)
+})
 test('Should render AddExpensePage correctly', () => {
-    const onSubmit = jest.fn();
-    const history = { push: jest.fn()}
-    const wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />)
     expect(wrapper).toMatchSnapshot()
 })
 
 test('Should handle Submit', () => {
-    const onSubmit = jest.fn();
-    const history = { push: jest.fn()}
-    const wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />)
+    onSubmit = jest.fn();
+    history = { push: jest.fn()}
+    wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />)
     wrapper.find('ExpenseForm').prop('onSubmit')(expenses[2])
     expect(history.push).toHaveBeenLastCalledWith('/')
     expect(onSubmit).toHaveBeenLastCalledWith(expenses[2])
