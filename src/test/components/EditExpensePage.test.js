73da198dcd6b5ad2b61b11b34dde794b4c65c137
Ribamar Santos <ribamaar@gmail.com>
2018-03-26 21:23:46 -0300

    Adding test render EditExpensePage

--- src/test/components/EditExpensePage.test.js
+++ src/test/components/EditExpensePage.test.js
@@ -1,1 +1,15 @@
-console.log('test')
+import React from 'react'
+import { shallow } from 'enzyme'
+import { EditExpensePage }from '../../components/EditExpensePage'
+import expenses from '../fixtures/expenses'
+
+let wrapper, onSubmit, expense;
+
+beforeEach(() => {
+    expense = expenses[2]
+    onSubmit = jest.fn()
+    wrapper = shallow(<EditExpensePage onSubmit={onSubmit} expense={expense} />)    
+})
+test('should render EditExpensePage correctly', () => {
+    expect(wrapper).toMatchSnapshot();
+})
