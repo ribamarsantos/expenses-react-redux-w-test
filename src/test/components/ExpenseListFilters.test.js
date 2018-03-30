332b87dada48fa3156f43a730afb0d9f154ed221
Ribamar Santos <ribamaar@gmail.com>
2018-03-30 11:18:15 -0300

    adding tests to expenselistfilters

--- src/test/components/ExpenseListFilters.test.js
+++ src/test/components/ExpenseListFilters.test.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import moment from 'moment'
 import { ExpenseListFilters } from '../../components/ExpenseListFilters'
 import { filters, altFilters } from '../fixtures/filters'
 
-let setTextFilter, setStartDate, setEndDate, setSortBy, wrapper;
+let setTextFilter, setStartDate, setEndDate, sortByDate, sortByAmount, wrapper;
 
 beforeEach(() => {
     setTextFilter = jest.fn();
-    setSortBy = jest.fn();
+    sortByDate = jest.fn();
+    sortByAmount = jest.fn();
     setStartDate = jest.fn();
     setEndDate = jest.fn();
     wrapper = shallow(
@@ -16,12 +18,13 @@ beforeEach(() => {
             setTextFilter={setTextFilter}
             setStartDate={setStartDate}
             setEndDate={setEndDate}
-            setSortBy={setSortBy}
+            sortByDate={sortByDate}
+            sortByAmount={sortByAmount}
         />
     )
 })
 
-test('Render ExpenseListFilter correectly', () => {
+test('Render ExpenseListFilter correctly', () => {
     expect(wrapper).toMatchSnapshot();
 });
 
@@ -31,3 +34,44 @@ test('Render ExpenseListFilter with data correctly', () => {
     });
     expect(wrapper).toMatchSnapshot();
 });
+
+test('should handle text change', () => {
+    const value = 'rent'
+    wrapper.find('input').simulate('change', {
+        target: { value }
+    });
+    expect(setTextFilter).toHaveBeenLastCalledWith(value)
+})
+
+test('should sort by date',() => {
+   const value = 'date';
+   wrapper.setProps({
+    filters: altFilters
+});   
+   wrapper.find('select').simulate('change', {
+       target: { value }
+   }) 
+   expect(sortByDate).toHaveBeenCalled();
+})
+
+test('should sort by amount', () => {
+    const value = 'amount';
+    wrapper.find('select').simulate('change', {
+        target: { value }
+    }) 
+    expect(sortByAmount).toHaveBeenCalled();
+})
+
+test('should handle date changes', () => {
+    const startDate = moment(0)
+    const endDate = moment().add(3,'days')
+    wrapper.find('DateRangePicker').prop('onDatesChange')({startDate, endDate});
+    expect(setStartDate).toHaveBeenLastCalledWith(startDate);
+    expect(setEndDate).toHaveBeenLastCalledWith(endDate);
+})
+
+test('should handle date focus changes', () => {
+    const calendarFocusedOn = 'endDate';
+    wrapper.find('DateRangePicker').prop('onFocusChange')( calendarFocusedOn )
+    expect(wrapper.state('calendarFocused')).toEqual(calendarFocusedOn)
+})
--- src/test/fixtures/filters.js
+++ src/test/fixtures/filters.js
@@ -9,7 +9,7 @@ const filters = {
 
 
 const altFilters = {
-    text: '',
+    text: 'bill',
     sortby: 'amount',
     startDate: moment(0),
     endDate: moment().add(3,'days')
