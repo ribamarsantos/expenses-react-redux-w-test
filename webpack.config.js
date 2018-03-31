8bf14648883dcf6e9905f2938231b3fc073826ba
Ribamar Santos <ribamaar@gmail.com>
2018-03-30 21:40:49 -0300

    removing comment package.json

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 module.exports = {
   entry: './src/app.js',
-  //entry: './src/playground/hoc.js',
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
