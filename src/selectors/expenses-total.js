656ba82619a59045df655979556707ebd7ea0aa5
Ribamar Santos <ribamaar@gmail.com>
2018-04-17 21:51:44 -0300

    Using TdD for new selector total expense

--- src/selectors/expenses-total.js
+++ src/selectors/expenses-total.js
@@ -1,2 +1,10 @@
 
-export default(expenses) =>  expenses.reduce( ( prevVal, elem ) => prevVal + elem.amount, 0 )/ 100; 
+
+// export default(expenses = [] ) => Array.isArray(expenses) ? 
+// expenses.reduce( ( sum, elem ) => sum + elem.amount, 0 )/ 100 : expenses.amount/100;
+
+export default (expenses) => { 
+    return expenses
+        .map(expense => (expense.amount))
+        .reduce((sum, value) => sum + value,0);
+}
--- src/test/selectors/expenses-total.test.js
+++ src/test/selectors/expenses-total.test.js
@@ -1,5 +1,20 @@
 import selectExpensesTotal from '../../selectors/expenses-total'
 import expenses from '../fixtures/expenses'
 
-const total = selectExpensesTotal(expenses);
-console.log(total);
+// const total = selectExpensesTotal(expenses);
+// console.log(total);
+
+test('should return 0 if no expenses', () => {
+    const total = selectExpensesTotal([]);
+    expect(total).toBe(0)
+});
+
+test('should correctly add up a single expense', () => {
+    const total = selectExpensesTotal([expenses[0]]);
+    expect(total).toEqual(expenses[0].amount)
+})
+
+test('should correctly add up multiples expenses', () => {
+    const total = selectExpensesTotal(expenses);
+    expect(total).toEqual(114195)
+})
