edb483491f4243d15a6335c70bf42ba5a7e1fbe2
Ribamar Santos <ribamaar@gmail.com>
2018-05-25 15:49:39 -0300

    removing HelpPage

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -5,7 +5,6 @@ import Header from '../components/Header';
 import ExpenseDashBoardPage from '../components/ExpenseDashBoardPage';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
-import HelpPage from '../components/HelpPage';
 import LoginPage from '../components/LoginPage'
 import NotFoundPage from '../components/NotFoundPage';
 import PrivateRoute from './PrivateRoute'
@@ -20,7 +19,6 @@ const AppRouter = () => (
             <PrivateRoute path="/dashboard" component={ExpenseDashBoardPage}    />
             <PrivateRoute path="/create" component={AddExpensePage}    />
             <PrivateRoute path="/edit/:id"   component={EditExpensePage}  />
-            <Route path="/help"   component={HelpPage}  />
             <Route component={NotFoundPage} />
         </Switch>
       </div>
