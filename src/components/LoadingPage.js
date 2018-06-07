ed6ef6febd2a22c0ba3e3def68265b5f2e54951b
Ribamar Santos <ribamaar@gmail.com>
2018-06-06 21:26:08 -0300

    Creating LoadingPage test

--- src/components/LoadingPage.js
+++ src/components/LoadingPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const LoadingPage = () => (
+export const LoadingPage = () => (
     <div className="loader">
         <img className="loader__image" src="/images/loader.gif" />
     </div>
